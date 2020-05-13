5b64a02fbd35f3a592c399a3f8e5854e973384af
Shevon Mendis <shevonmendis98@gmail.com>
2020-05-14 02:50:16 +1000

    Add music videos to page

--- App/src/pages/music.js
+++ App/src/pages/music.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import Layout from '../components/layout';
 
-function music() {
+import "../stylesheets/music.css";
+
+const music = () => {
     return (
-        <div>
+        <Layout>
+            <div
+                className="music-page">
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube-nocookie.com/embed/a2CVi2pXNxA"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+                <iframe
+                    width="560"
+                    height="315"
+                    src="https://www.youtube-nocookie.com/embed/EDajeXIr6Jg"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            </div>
+        </Layout>
 
-        </div>
     )
 }
 
-export default music
+export default music;
+
+
