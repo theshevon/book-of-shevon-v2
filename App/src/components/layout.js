3eaf952b86a140ea99f4944e3317cfa33869c381
Shevon Mendis <shevonmendis98@gmail.com>
2020-05-14 17:39:08 +1000

    Add necessary dependencies

--- App/src/components/layout.js
+++ App/src/components/layout.js
@@ -13,6 +13,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./layout.css"
 import Nav from "./Navbar/MainNavBar"
 
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
