6caf334eae1892b27468c65f6486d9a58f78eb41
theshevon <shevonmendis98@gmail.com>
2021-07-06 22:45:35 +1000

    Add messages for experiences

---
+++ App/src/pages/about/info/experiences/experiences.messages.ts
@@ -0,0 +1,40 @@
+const ExperiencesMessages = {
+
+  SoftwareEngineer: (): string => 'Software Engineer',
+
+  Canva: (): string => 'Canva',
+
+  April: (): string => 'Apr',
+
+  Present: (): string => 'Present',
+
+  CanvaResponsibility1: (): string => 'Building and maintaing UI components',
+
+  CanvaResponsibility2: (): string => 'Performing code reviews',
+
+  DgitSystems: (): string => 'DGiT Systems',
+
+  July: (): string => 'Jul',
+
+  March: (): string => 'Mar',
+
+  DgitResponsibility1: (): string => 'Designing, building and testing platform-based solutions that meet clients',
+
+  DgitResponsibility2: (): string => 'Winning the ”Best Rookie” (2020) Award',
+
+  UniMelb: (): string => 'The University of Melbourne',
+
+  May: (): string => 'May',
+
+  December: (): string => 'Dec',
+
+  UnimelbEngResponsibility1: (): string => 'Developing a web app that would allow individuals to visualise and analyse data sourced from COVID-19 literature',
+
+  UnimelbEngResponsibility2: (): string => 'Integrating indiviudally developed analysis/ visualisation tools into a web server that would act as the API for the client app',
+
+  AcademicTutor: (): string => 'Academic Tutor',
+
+  UnimelbTutResponsibility1: (): string => 'Assisting students in learning and understanding programming concepts in Python, both in class (in sizes of 25 - 30) and via Grok monitoring',
+
+  UnimelbTutResponsibility2: (): string => 'Marking students\' projects and exams',
+}
