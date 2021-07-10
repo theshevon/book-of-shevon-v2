c359013f8c24f9241b0796454ece7e8652533f64
theshevon <shevonmendis98@gmail.com>
2021-07-10 23:03:29 +1000

    Ordered about experience messages

--- App/src/pages/about/info/experiences/experiences.messages.ts
+++ App/src/pages/about/info/experiences/experiences.messages.ts
@@ -1,10 +1,33 @@
 export const ExperiencesMessages = {
 
+  /* 3 character abbreviation for the month of March */
+  March: (): string => 'Mar',
+
+  /* 3 character abbreviation for the month of April */
+  April: (): string => 'Apr',
+
+  /* 3 character abbreviation for the month of May */
+  May: (): string => 'May',
+
+  /* 3 character abbreviation for the month of July */
+  July: (): string => 'Jul',
+
+  /* 3 character abbreviation for the month of September */
+  September: (): string => 'Sep',
+
+  /* 3 character abbreviation for the month of October */
+  October: (): string => 'October',
+
+  /* 3 character abbreviation for the month of December */
+  December: (): string => 'Dec',
+
+  /* Software Engineer */
   SoftwareEngineer: (): string => 'Software Engineer',
 
+  /* Member of the General Committee */
+  GenCommMember: (): string => 'General Committee Member',
+  
   Canva: (): string => 'Canva',
-
-  April: (): string => 'Apr',
 
   CanvaResponsibility1: (): string => 'Building and maintaing UI components',
 
@@ -12,19 +35,11 @@ export const ExperiencesMessages = {
 
   DgitSystems: (): string => 'DGiT Systems',
 
-  July: (): string => 'Jul',
-
-  March: (): string => 'Mar',
-
   DgitResponsibility1: (): string => 'Designing, building and testing platform-based solutions that meet clients',
 
   DgitResponsibility2: (): string => 'Winning the ”Best Rookie” (2020) Award',
 
   UniMelb: (): string => 'The University of Melbourne',
-
-  May: (): string => 'May',
-
-  December: (): string => 'Dec',
 
   UniMelbEngResponsibility1: (): string => 'Developing a web app that would allow individuals to visualise and analyse data sourced from COVID-19 literature',
 
@@ -40,17 +55,11 @@ export const ExperiencesMessages = {
 
   Codec: (): string => 'CODEC (Community for Digital Entertainment Creation)',
 
-  October: (): string => 'October',
-
   CodecResponsibility1: (): string => 'Inaugural member of the committee that accumulated over 150 memberships in its first semester and was awarded the title of \'Best New Club\' by the University of Melbourne\'s Student Union', 
   
   CodecResponsibility2: (): string => 'Touched base with every aspect of the club including designing the banner and logo, as well as in planning workshops for students interested in animation and game design',
 
-  GenCommMember: (): string => 'General Committee Member',
-
-  Misc: (): string => 'UMISC (University of Melbourne Information Security Club)',
-
-  September: (): string => 'Sep',
+  Misc: (): string => 'MISC (University of Melbourne Information Security Club)',
 
   MiscResponsibility1: (): string => 'Member of the committee that revived the club from dormancy and attained a 12% increase in club memberships as well as a steep rise in engagement compared to the previous year',
 
