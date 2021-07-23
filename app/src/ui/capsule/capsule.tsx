e498e59fdbaa8957e9d2120ef08a38baa8bdd882
theshevon <shevonmendis98@gmail.com>
2021-07-23 23:48:21 +1000

    Refactor capsule to support links

--- app/src/ui/capsule/capsule.tsx
+++ app/src/ui/capsule/capsule.tsx
@@ -1,20 +1,45 @@
 import React from 'react'
 import classNames from 'classnames';
 
+import { Text } from './../text/text';
+
 import styles from './capsule.module.css';
 
+type Link = {
+  anchorText: string,
+  url: string,
+}
+
+type Content = string | Link;
+
 type CapsuleProps = {
-  text: string,
+  content: Content,
   className?: string[],
 }
 
+const renderContent = (content: Content) => {
+  if (typeof content === 'string') {
+    return <Text.Small>{content}</Text.Small>
+  }
+  return (
+    <a
+        href={content.url}
+        target='_blank'
+        rel='noreferrer'
+        className={styles.link}
+    >
+      {content.anchorText}
+    </a>
+  );
+}
+
 export const Capsule = ({
-  text,
+  content,
   className = [],
 }: CapsuleProps) => (
   <div
       className={classNames(styles.capsule, ...className)}
   >
-    {text}
+    {renderContent(content)}
   </div>
 );
