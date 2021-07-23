791747e688df30c20e6aed90b760cec25411df00
theshevon <shevonmendis98@gmail.com>
2021-07-24 00:08:32 +1000

    Set up messages for projects

---
+++ app/src/pages/projects/info/project/project.ts
@@ -0,0 +1,94 @@
+export const ProjectMessages = {
+
+  InvoiceMaker: (): string => 'Invoice Maker',
+
+  InvoiceMakerDesc: (): string => 'A script to automate invoicing based on information stored in a Google Sheet.',
+
+  BookOfShevon: (): string => 'Book of Shevon',
+
+  BookOfShevonDesc: (): string => 'Current portfolio website.',
+
+  MiscWebsite: (): string => 'MISC Website',
+
+  MiscWebsiteDesc: (): string => 'Former (and inaugral) website for the University of Melbourne\'s Information Security Club.',
+
+  LubyJump: (): string => 'Luby Jump',
+
+  LubyJumpDesc: (): string => 'A \'Flappy Bird\'-style mobile game. The \'pipes\' in this game are dynamic and so, make the game quite difficult to beat. The current high score sits at 115.',
+
+  TheTreasurePlatform: (): string => 'The Treasure Platform',
+
+  TheTreasurePlatformDesc: (): string => 'A web application that would allow an individual to effectively catalogue family artefacts that they may want to pass down the generations over the years.',
+
+  ReviewMyResume: (): string => 'ReviewMyResume',
+
+  ReviewMyResumeDesc: (): string => 'A web application that would allow students to upload and receive feedback on their resumes from other users on the site, which included dedicated reviewers.',
+
+  TheAdventuresOfSquidwod: (): string => 'The Adventures of Squidwod',
+  
+  TheAdventuresOfSquidwodDesc: (): string => 'A Unity game that involves collecting Eggs and fighting a giant fire-breathing, laser-eyed, grenade-tossing Seagull.',
+
+  LearningToEscape: (): string => 'Learning to Escape',
+
+  LearningToEscapeDesc: (): string => 'An AI controller that navigates a car through a maze in such a way that it collects the required keys to escape while simultaneously avoiding traps.',
+
+  LandscapeGenerator: (): string => 'Landscape Generator',
+
+  LandscapeGeneratorDesc: (): string => 'A Unity project that dynamically generates a stochastic landscape, inclusive of a terrain, a body of water and an orbiting sun.',
+
+  Python: (): string => 'Python',
+  
+  C: (): string => 'C',
+
+  CSharp: (): string => 'C#',
+  
+  Java: (): string => 'Java',
+  
+  Html: (): string => 'HTML', 
+  
+  Css: (): string => 'CSS', 
+  
+  JavaScript: (): string => 'JavaScript', 
+    
+  React: (): string => 'React',
+
+  Redux: (): string => 'Redux',
+
+  MobX: (): string => 'MobX',
+  
+  MongoDb: (): string => 'MongoDB', 
+  
+  MySql: (): string => 'MySQL', 
+  
+  Git: (): string => 'Git', 
+
+  Haskell: (): string => 'Haskell', 
+
+  Prolog: (): string => 'Prolog', 
+  
+  Firebase: (): string => 'Firebase', 
+  
+  Swift: (): string => 'Swift',
+
+  JQuery: (): string => 'JQuery',
+
+  Node: (): string => 'Node',
+
+  AndroidStudio: (): string => 'Android Studio',
+
+  Unity: (): string => 'Unity',
+
+  AdamTurner: (): string => 'Adam Turner',
+
+  GraceSng: (): string => 'Grace Sng',
+
+  MichaelManoussakis: (): string => 'Michael Manoussakis',
+
+  AmieXie: (): string => 'Amie Xie',
+
+  AndrewGourley: (): string => 'Andrew Gourley',
+
+  DavidCrowe: (): string => 'David Crowe',
+
+  BrendenLeung: (): string => 'Brenden Leung',
+}
