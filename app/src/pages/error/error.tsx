590ec22d2b466caf1a4199f306fdb8bfcb868982
theshevon <shevonmendis98@gmail.com>
2021-09-05 21:27:10 +1000

    Optimise rendering

--- app/src/pages/error/error.tsx
+++ app/src/pages/error/error.tsx
@@ -9,49 +9,64 @@ import { CARDS_SPRITE } from './cards_sprite';
 const POSITION_UPDATE_FREQ_PER_S = 60;
 const POSITION_UPDATE_INTERVAL = 1000 / POSITION_UPDATE_FREQ_PER_S;
 
-const N_INITIAL_CARDS = 10; 
+const MIN_INITIAL_CARDS = 5;
+const MAX_INITIAL_CARDS = 15; 
 const AUTO_ADDITION_UPDATE_INTERVAL = 2000;
 
 const N_CARDS_IN_DECK = 52;
+const N_SUITES = 4;
 const CARD_WIDTH = 71;
 const CARD_WIDTH_HALF = CARD_WIDTH / 2;
 const CARD_HEIGHT = 96;
 const CARD_HEIGHT_HALF = CARD_HEIGHT / 2;
 
+/**
+ * This implementation is based on the one the Winning Solitaire implementation by Ricardo Cabello
+ * (https://mrdoob.com/lab/javascript/effects/solitaire/)
+ */
 class Card {
   
-  private x: number;
-  private y: number;
+  private image: HTMLImageElement;
+  private currX: number;
+  private currY: number;
+  private prevX: number;
+  private prevY: number;
   private dx: number;
   private dy: number;
-  private image: HTMLImageElement;
   private sx: number;
   private sy: number;
 
-  constructor(id: number, x: number, y: number, dx: number, dy: number, image: HTMLImageElement) {
-    this.x = x;
-    this.y = y;
+  constructor(image: HTMLImageElement, id: number, x: number, y: number, dx: number, dy: number) {
+    this.image = image;
+    this.currX = x;
+    this.currY = y;
+    this.prevX = x;
+    this.prevY = y;
     this.dx = dx;
     this.dy = dy;
-    this.image = image;
-    this.sx = (id % 4) * CARD_WIDTH;
-    this.sy = Math.floor(id / 4) * CARD_HEIGHT;
+    this.sx = (id % N_SUITES) * CARD_WIDTH;
+    this.sy = Math.floor(id / N_SUITES) * CARD_HEIGHT;
   }
 
   updatePosition(canvasHeight: number) {
-    this.x += this.dx;
-    this.y += this.dy;
+    this.prevX = this.currX;
+    this.prevY = this.currY;
 
-    if (this.y > canvasHeight - CARD_HEIGHT_HALF) {
-      this.y = canvasHeight - CARD_HEIGHT_HALF;
-      this.dy = -(this.dy *  0.85);
+    this.currX += this.dx;
+    this.currY += this.dy;
+
+    if (this.currY > canvasHeight - CARD_HEIGHT_HALF) {
+      this.currY = canvasHeight - CARD_HEIGHT_HALF;
+      this.dy = -(this.dy *  0.85); // TODO
     }
 
-    this.dy += 0.98;
+    this.dy += 0.98; // TODO
   }
  
-  isOutOfView(canvasWidth: number) {
-    return this.x < -CARD_WIDTH_HALF || this.x > (canvasWidth + CARD_WIDTH_HALF);
+  isActiveWithinCanvas(canvasWidth: number) {
+    const isOutOfView = this.currX < -CARD_WIDTH_HALF || this.currX > (canvasWidth + CARD_WIDTH_HALF);
+    const hasStoppedMoving = this.currX === this.prevX && this.currY === this.prevY;
+    return !(isOutOfView || hasStoppedMoving);
   }
 
   draw(ctx: CanvasRenderingContext2D) {
@@ -61,8 +76,8 @@ class Card {
       this.sy,
       CARD_WIDTH,
       CARD_HEIGHT,
-      Math.floor(this.x - CARD_WIDTH_HALF),
-      Math.floor(this.y - CARD_HEIGHT_HALF),
+      Math.floor(this.currX - CARD_WIDTH_HALF),
+      Math.floor(this.currY - CARD_HEIGHT_HALF),
       CARD_WIDTH,
       CARD_HEIGHT,
     );
@@ -79,14 +94,14 @@ export const Error = () => {
 
   const [canvasWidth, setCanvasWidth] = useState<number>();
   const [canvasHeight, setCanvasHeight] = useState<number>();
-  const [cards, setCards] = useState<Card[]>(Array.from({ length: N_INITIAL_CARDS }, () => {
+  const [cards, setCards] = useState<Card[]>(Array.from({ length: Math.floor(Math.random() * (MAX_INITIAL_CARDS - MIN_INITIAL_CARDS)) + MIN_INITIAL_CARDS  }, () => {
     return new Card(
+      image,
       Math.floor(Math.random() * (N_CARDS_IN_DECK - 1)),
       Math.random() * window.innerWidth,
       -CARD_HEIGHT,
-      Math.floor(Math.random() * 6 - 3 ) * 2,
-      -Math.random() * 16,
-      image,
+      Math.floor(Math.random() * 6 - 3) * 2, // TODO
+      -Math.random() * 16, // TODO
     );
   }));
 
@@ -94,12 +109,12 @@ export const Error = () => {
     setCards([
       ...cards,
       new Card(
+        image,
         Math.floor(Math.random() * (N_CARDS_IN_DECK - 1)),
         x,
         y,
-        Math.floor(Math.random() * 6 - 3 ) * 2,
-        -Math.random() * 16, 
-        image
+        Math.floor(Math.random() * 6 - 3) * 2,
+        -Math.random() * 16,
       ),
     ]);
   }
@@ -110,16 +125,16 @@ export const Error = () => {
     for (let i=0; i<cards.length; i++) {
       const card = cards[i];
       card.updatePosition(canvas.height);
-      if (!card.isOutOfView(canvas.width)) {
+      if (card.isActiveWithinCanvas(canvas.width)) {
         card.draw(renderingCtx);
       }
     }
 
     // remove all cards that are out of the view
     for (let i=0; i<cards.length; i++) {
-      const cardsStillInView = cards.filter(card => !card.isOutOfView(canvas.width));
-      if (cardsStillInView.length !== cards.length) {
-        setCards(cardsStillInView);
+      const activeCards = cards.filter(card => card.isActiveWithinCanvas(canvas.width));
+      if (activeCards.length !== cards.length) {
+        setCards(activeCards);
       }
     }
   }
