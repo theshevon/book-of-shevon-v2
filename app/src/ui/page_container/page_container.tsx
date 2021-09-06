662189650f54ba258e5058f9d7485cdcb24ecf9a
theshevon <shevonmendis98@gmail.com>
2021-09-06 23:37:00 +1000

    Remove aos for projects page

--- app/src/ui/page_container/page_container.tsx
+++ app/src/ui/page_container/page_container.tsx
@@ -13,6 +13,7 @@ export const PageContainer = ({
   <div
       className={styles.pageContainer}
   >
+    {/* TODO: The UI should be agnostic to the data. Therefore, the route data should be passed into this component  */}
     <Nav
         routes={routes}
         activeRoute={window.location.pathname}
