f0a8e5f59699cdf9400a389782897a41e49a4b62
theshevon <shevonmendis98@gmail.com>
2021-10-11 00:10:28 +1100

    Move IconLink component to ui/link

--- app/src/ui/link/link.tsx
+++ app/src/ui/link/link.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import classNames from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
 import styles from './link.module.css';
 
@@ -7,6 +9,10 @@ export type LinkProps = {
   url: string,
   targetSelf?: boolean,
   className?: string,
+}
+
+export type IconLinkProps = Omit<LinkProps, 'className'> & {
+	icon: IconDefinition,	
 }
 
 export const Link: React.FC<LinkProps> = ({
@@ -53,5 +59,19 @@ export const CapsuleLink: React.FC<LinkProps> = ({
       className={classNames(styles.capsuleLink, className)}
   >
     {children}
+  </Link>
+);
+
+export const IconLink = ({
+	icon,
+  ...linkProps
+}: IconLinkProps) => (
+  <Link
+      {...linkProps}
+      className={styles.iconLink}
+  >
+    <FontAwesomeIcon
+        icon={icon}
+    />
   </Link>
 );
