36dd3fdd0144fbf96078cd1e1a046eb6290f5409
theshevon <shevonmendis98@gmail.com>
2021-10-20 23:20:55 +1100

    Delete blog header img

---
+++ app/src/pages/blog/blog_header/blog_header.messages.ts
@@ -0,0 +1,6 @@
+export const BlogHeaderMessages = {
+
+  title: (): string => 'Blog',
+
+  subtitle: (): string => 'Thoughts and stories [which do not reflect those of my employer(s)]',
+}
--- app/src/pages/blog/blog_header/blog_header.tsx
+++ app/src/pages/blog/blog_header/blog_header.tsx
@@ -2,24 +2,19 @@ import React from 'react';
 
 import { Text } from './../../../ui/text/text';
 
-import { BlogMessages as Messages } from './../blog.messages';
+import { BlogHeaderMessages as Messages } from './blog_header.messages';
 
-import HeaderImg from './blog_header_img.jpeg';
 import styles from './blog_header.module.css';
 
 export const BlogHeader = () => (
   <div
       className={styles.blogHeader}
   >
-    <img
-        className={styles.backgroundImg}
-        src={HeaderImg}
-        alt='lofi girl'
-    />
     <div
         className={styles.textContainer}
     >
-      <Text.LargeTitle>{Messages.pageTitle()}</Text.LargeTitle>
+      <Text.LargeTitle>{Messages.title()}</Text.LargeTitle>
+      <Text.Small>{Messages.subtitle()}</Text.Small>
     </div>
   </div>
 );
