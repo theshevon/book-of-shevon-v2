b654398c378bcbde472644f08ed835ca1ed93be2
theshevon <shevonmendis98@gmail.com>
2021-10-24 02:32:04 +1100

    Finalise blog header design

--- app/src/pages/blog/blog_header/blog_header.messages.ts
+++ app/src/pages/blog/blog_header/blog_header.messages.ts
@@ -2,7 +2,5 @@ export const BlogHeaderMessages = {
 
   title: (): string => 'Blog',
 
-  subtitle: (): string => 'Literature and opinions on things I find interesting',
-
-  disclaimer: (): string => 'and do not reflect those of my employer(s)',
+  subtitle: (): string => 'Essays, poetry and opinions on things I find interesting',
 }
