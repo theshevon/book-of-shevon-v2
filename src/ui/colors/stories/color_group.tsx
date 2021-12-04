78fd55404b7af2893faf869ade8bdfd1c1454dc9
Shevon Mendis <shevonmendis98@gmail.com>
2021-12-04 23:21:19 +1100

    Organise colors into design system

---
+++ src/ui/colors/stories/color_group.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text } from '../../text/text';
+import styles from './color_group.module.css';
+
+type ColorRectProps = {
+	name: string,
+	value: string,
+};
+
+export const ColorRect = ({
+  name,
+  value,
+}: ColorRectProps) => (
+  <div
+      className={styles.colorRect}
+      style={{ backgroundColor: value }}
+  >
+    <div
+        className={styles.colorLabel}
+    >
+      <Text.ExtraSmall>
+        { name }
+      </Text.ExtraSmall>
+    </div>
+  </div>
+);
+
+export type ColorGroupProps = {
+	name: string,
+	colors: ColorRectProps[],
+};
+
+export const ColorGroup = ({
+  name,
+  colors,
+}: ColorGroupProps) => (
+  <div
+      className={styles.colorGroup}
+  >
+    <Text.Small
+        fontWeight='bold'
+        textCase='uppercase'
+        keepDefaultMargins={true}
+    >
+      { name }
+    </Text.Small>
+    <div
+        className={styles.colors}
+    >
+      { colors.map(color => (
+        <ColorRect
+            {...color}
+        />
+      )) }
+    </div>
+  </div>
+);
---
+++ src/ui/colors/stories/colors.stories.tsx
@@ -0,0 +1,61 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import colorValues from '../colors.module.css';
+import { ColorGroup, ColorGroupProps } from './color_group';
+
+const COLOR_GROUPS: ColorGroupProps[] = [
+  {
+    name: 'Grey',
+    colors: [
+      {
+        name: 'colorGrey100',
+        value: colorValues.colorGrey100,
+      },
+      {
+        name: 'colorGrey200',
+        value: colorValues.colorGrey200,
+      },
+      {
+        name: 'colorGrey300',
+        value: colorValues.colorGrey300,
+      },
+      {
+        name: 'colorGrey400',
+        value: colorValues.colorGrey400,
+      },
+      {
+        name: 'colorGrey500',
+        value: colorValues.colorGrey500,
+      },
+      {
+        name: 'colorGreyTransparentLight',
+        value: colorValues.colorGreyTransparentLight,
+      },
+      {
+        name: 'colorGreyTransparentDark',
+        value: colorValues.colorGreyTransparentDark,
+      },
+    ],
+  },
+  {
+    name: 'Flat Gold',
+    colors: [
+      {
+        name: 'colorFlatGold',
+        value: colorValues.colorFlatGold,
+      },
+    ],
+  },
+];
+
+storiesOf('UI/Colors', module)
+  .add('Colors', () => (
+    <div
+        style={{
+          display: 'flex',
+          columnGap: '10px',
+        }}
+    >
+      { COLOR_GROUPS.map(colorGroup => <ColorGroup {...colorGroup}/>) }
+    </div>
+  ));
