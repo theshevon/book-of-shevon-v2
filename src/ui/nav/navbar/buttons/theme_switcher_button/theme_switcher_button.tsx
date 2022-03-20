fd1592dbe91c4e42abe0ed841c0d92a8eafaa423
Shevon Mendis <shevonmendis98@gmail.com>
2022-03-21 00:32:19 +1100

    Disabled bg color change on hover for theme switcher button

--- src/ui/nav/navbar/buttons/theme_switcher_button/theme_switcher_button.tsx
+++ src/ui/nav/navbar/buttons/theme_switcher_button/theme_switcher_button.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import React from 'react';
 import { THEMES, Theme, useThemeContext } from '../../../../../util/theming/theme_provider';
 import { Button } from '../../../../button/button';
-import navbarButtonStyles from './../navbar_button.module.css';
+import baseStyles from './../navbar_button.module.css';
 import themeSwitcherButtonStyles from './theme_switcher_button.module.css';
 
 export const ThemeSwitcherButton = () => {
@@ -10,7 +10,7 @@ export const ThemeSwitcherButton = () => {
   return (
     <Button
         onClick={() => {}}
-        className={classNames(navbarButtonStyles.navbarButton, {
+        className={classNames(baseStyles.navbarButton, themeSwitcherButtonStyles.themeSwitcherButton, {
           [themeSwitcherButtonStyles.eightBit]: theme === Theme.EIGHT_BIT,
         })}
     >
@@ -20,6 +20,7 @@ export const ThemeSwitcherButton = () => {
         { THEMES.map(themeOption => (
           <div
               key={themeOption.theme.toString()}
+              title={themeOption.label}
               onClick={() => setTheme(themeOption.theme)}
               className={classNames(themeSwitcherButtonStyles.themeOption, {
                 [themeSwitcherButtonStyles.active]: themeOption.theme === theme,
