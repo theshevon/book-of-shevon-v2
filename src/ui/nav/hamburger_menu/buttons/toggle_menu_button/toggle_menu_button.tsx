8fbc4355a9fa16a6e0ad4a1da3a320c44eb628d6
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-12 20:22:06 +1100

    Change hamburger button colour on dark mode

--- src/ui/nav/hamburger_menu/buttons/toggle_menu_button/toggle_menu_button.tsx
+++ src/ui/nav/hamburger_menu/buttons/toggle_menu_button/toggle_menu_button.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames';
 import React from 'react';
+import { Appearance, useThemeContext } from '../../../../../util/theming/theme_provider';
 import styles from './toggle_menu_button.module.css';
 
 type ToggleMenuButtonProps = {
@@ -10,19 +11,24 @@ type ToggleMenuButtonProps = {
 export const ToggleMenuButton = ({
   menuOpen,
   onClick,
-}: ToggleMenuButtonProps) => (
-  <div
-      role='button'
-      className={classnames(styles.toggleMenuButton, { [styles.menuOpen]: menuOpen })}
-      onClick={onClick}
-  >
+}: ToggleMenuButtonProps) => {
+  const { appearance } = useThemeContext();
+  return (
     <div
-        className={classnames(styles.line, { [styles.menuOpen]: menuOpen })}
+        role='button'
+        className={classnames(styles.toggleMenuButton, {
+          [styles.menuOpen]: menuOpen,
+          [styles.dark]: appearance === Appearance.DARK,
+        })}
+        onClick={onClick}
     >
+      <div
+          className={classnames(styles.line, { [styles.menuOpen]: menuOpen })}
+      >
+      </div>
+      <div
+          className={classnames(styles.line, { [styles.menuOpen]: menuOpen })}
+      >
+      </div>
     </div>
-    <div
-        className={classnames(styles.line, { [styles.menuOpen]: menuOpen })}
-    >
-    </div>
-  </div>
-);
+  );};
