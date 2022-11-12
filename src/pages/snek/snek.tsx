5b5be86841fbfee6d3213315e2e1b8001cd1a8a1
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-12 19:11:14 +1100

    Update button bg colour in dark mode

--- src/pages/snek/snek.tsx
+++ src/pages/snek/snek.tsx
@@ -1,6 +1,8 @@
+import classNames from 'classnames';
 import React, { useEffect } from 'react';
 import { Button } from '../../ui/button/button';
 import { PageContainer } from '../../ui/page_container/page_container';
+import { Appearance, useThemeContext } from '../../util/theming/theme_provider';
 import { updateDocumentHeader } from '../../util/title_manager';
 import { Text } from './../../ui/text/text';
 import { runGame } from './game';
@@ -16,28 +18,69 @@ export const Snek = () => {
     runGame();
   }, []);
 
+  const { appearance } = useThemeContext();
+
   const Content = () => (
     <>
-      <div className={styles.gameWelcome}>
-        <Text.LargeTitle alignment='centre' fontWeight='bold'>🐍 Welcome to Favicon SnEk 🐍</Text.LargeTitle>
+      <div
+          className={styles.gameWelcome}
+      >
+        <Text.LargeTitle
+            alignment='centre'
+            fontWeight='bold'
+        >
+          🐍 Welcome to Favicon SnEk 🐍
+        </Text.LargeTitle>
       </div>
-      <div className={styles.gameInstructions}>
-        <Text.Small alignment='centre'>Use the arrow keys to control the snek</Text.Small>
+      <div
+          className={styles.gameInstructions}
+      >
+        <Text.Small
+            alignment='centre'
+        >
+          Use the arrow keys to control the snek
+        </Text.Small>
       </div>
-      <div className={styles.gameControls}>
-        <div className={styles.topRow}>
-          <Button onClick={noop} className={styles.gameControlBtn}>
+      <div
+          className={styles.gameControls}
+      >
+        <div
+            className={styles.topRow}
+        >
+          <Button
+              onClick={noop}
+              className={classNames(styles.gameControlBtn, {
+                [styles.dark]: appearance === Appearance.DARK,
+              })}
+          >
             ⬆️
           </Button>
         </div>
-        <div className={styles.bottomRow}>
-          <Button onClick={noop} className={styles.gameControlBtn}>
+        <div
+            className={styles.bottomRow}
+        >
+          <Button
+              onClick={noop}
+              className={classNames(styles.gameControlBtn, {
+                [styles.dark]: appearance === Appearance.DARK,
+              })}
+          >
             ⬅️
           </Button>
-          <Button onClick={noop} className={styles.gameControlBtn}>
+          <Button
+              onClick={noop}
+              className={classNames(styles.gameControlBtn, {
+                [styles.dark]: appearance === Appearance.DARK,
+              })}
+          >
             ⬇️
           </Button>
-          <Button onClick={noop} className={styles.gameControlBtn}>
+          <Button
+              onClick={noop}
+              className={classNames(styles.gameControlBtn, {
+                [styles.dark]: appearance === Appearance.DARK,
+              })}
+          >
             ➡️
           </Button>
         </div>
