98aa6a164c826e9d7cb3715a6065873ce26f271e
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-12 16:13:07 +1100

    Stop flashing when loading page in dark mode

--- src/ui/preload/page_placeholder/page_placeholder.tsx
+++ src/ui/preload/page_placeholder/page_placeholder.tsx
@@ -1,4 +1,16 @@
+import classNames from 'classnames';
 import React from 'react';
+import { Appearance, useThemeContext } from '../../../util/theming/theme_provider';
 import styles from './page_placeholder.module.css';
 
-export const PagePlaceholder = () => <div className={styles.spinner}></div>;
+export const PagePlaceholder = () => {
+  const { appearance } = useThemeContext();
+  return (
+    <div className={classNames(styles.container, {
+      [styles.dark]: appearance === Appearance.DARK,
+    })}>
+      <div className={styles.spinner}></div>
+    </div>
+  );
+};
+
