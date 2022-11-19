f7149b4df63e6cfed19f1950ad4cd58e2c903a27
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-20 01:46:20 +1100

    Style mobile menu button with mario theme

--- src/ui/nav/hamburger_menu/buttons/toggle_menu_button/stories/toggle_menu_button.stories.tsx
+++ src/ui/nav/hamburger_menu/buttons/toggle_menu_button/stories/toggle_menu_button.stories.tsx
@@ -2,12 +2,31 @@ import { storiesOf } from '@storybook/react';
 import React, { useState } from 'react';
 import { ToggleMenuButton } from '../toggle_menu_button';
 
+const routesData = [
+  {
+    label: 'Route 1',
+    path: '#',
+    index: -1,
+  },
+  {
+    label: 'Route 2',
+    path: '##',
+    index: 0,
+  },
+  {
+    label: 'Route 3',
+    path: '#',
+    index: 1,
+  },
+];
+
 storiesOf('UI/Nav/HamburgerMenu/Buttons', module)
   .add('ToggleMenuButton', () => {
     const [menuOpen, setMenuOpen] = useState<boolean>(false);
     const toggleMenu = () => setMenuOpen(!menuOpen);
     return (
       <ToggleMenuButton
+          routesData={routesData}
           menuOpen={menuOpen}
           onClick={toggleMenu}
       />
--- src/ui/nav/hamburger_menu/buttons/toggle_menu_button/toggle_menu_button.tsx
+++ src/ui/nav/hamburger_menu/buttons/toggle_menu_button/toggle_menu_button.tsx
@@ -1,25 +1,36 @@
 import classnames from 'classnames';
 import React from 'react';
-import { Appearance, useThemeContext } from '../../../../../util/theming/theme_provider';
+import { useLocation } from 'react-router-dom';
+import { RouteData } from '../../../../../routes/route_data/route_data';
+import { Routes } from '../../../../../routes/routes';
+import { Appearance, Theme, useThemeContext } from '../../../../../util/theming/theme_provider';
 import styles from './toggle_menu_button.module.css';
 
 type ToggleMenuButtonProps = {
+  routesData: RouteData[],
   menuOpen: boolean,
   onClick: () => void,
 }
+
+const getLocationSpecificThemeStyles = (activeRoute: string, theme: Theme,) => {
+  if (activeRoute === Routes.PROJECTS && theme === Theme.EIGHT_BIT) {
+    return styles.projects;
+  }
+};
 
 export const ToggleMenuButton = ({
   menuOpen,
   onClick,
 }: ToggleMenuButtonProps) => {
-  const { appearance } = useThemeContext();
+  const { pathname: activeRoute } = useLocation();
+  const { appearance, theme } = useThemeContext();
   return (
     <div
         role='button'
         className={classnames(styles.toggleMenuButton, {
           [styles.menuOpen]: menuOpen,
           [styles.dark]: appearance === Appearance.DARK,
-        })}
+        }, getLocationSpecificThemeStyles(activeRoute, theme))}
         onClick={onClick}
     >
       <div
