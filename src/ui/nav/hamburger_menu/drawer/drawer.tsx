63973947e4b0dae74df989c414173e0a0b508c5d
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-19 23:40:39 +1100

    Add section label for theme and appearance options

--- src/ui/nav/hamburger_menu/drawer/drawer.tsx
+++ src/ui/nav/hamburger_menu/drawer/drawer.tsx
@@ -51,59 +51,78 @@ export const Drawer = ({
           </li>
         )) }
       </ul>
-      <ThemeSwitcherOptions/>
+      <div
+          className={styles.horizontalDivider}
+      >
+      </div>
+      <ThemeAppearanceOptions/>
     </div>
   );
 };
 
-const ThemeSwitcherOptions = () => {
+const ThemeAppearanceOptions = () => {
 
   const { theme, setTheme, appearance, setAppearance } = useThemeContext();
 
   return (
     <div
-        className={classNames(styles.themeOptions, {
-          [styles.dark]: appearance === Appearance.DARK,
-        })}
+        className={styles.themeAppearanceOptionsContainer}
     >
-      { THEMES.map(themeOption => (
-        <ThemeAppearanceSwitcherButton
-            key={themeOption.theme.toString()}
-            title={themeOption.label}
-            onClick={() => setTheme(themeOption.theme)}
-            className={classNames(styles.themeOption, {
-              [styles.active]: themeOption.theme === theme,
-            })}
-            icon={themeOption.icon}
+      <Text.ExtraSmall
+          textCase='uppercase'
+          alignment='centre'
+      >
+        Themes / Appearance
+      </Text.ExtraSmall>
+      <div
+          className={classNames(styles.themeAppearanceOptions, {
+            [styles.dark]: appearance === Appearance.DARK,
+          })}
+      >
+        { THEMES.map(themeOption => (
+          <ThemeAppearanceButton
+              key={themeOption.theme.toString()}
+              label={themeOption.label}
+              onClick={() => setTheme(themeOption.theme)}
+              icon={themeOption.icon}
+              className={styles.themeOption}
+              active={themeOption.theme === theme}
+          />
+        )) }
+        <div
+            className={styles.verticalDivider}
+        >
+        </div>
+        <ThemeAppearanceButton
+            label={APPEARANCES[appearance].label}
+            onClick={() => setAppearance(appearance === Appearance.LIGHT ? Appearance.DARK : Appearance.LIGHT)}
+            icon={appearance === Appearance.LIGHT ? APPEARANCES[Appearance.DARK].icon : APPEARANCES[Appearance.LIGHT].icon}
+            className={styles.appearanceOption}
         />
-      )) }
-      <div className={styles.verticalDivider}></div>
-      <ThemeAppearanceSwitcherButton
-          title={APPEARANCES[appearance].label}
-          onClick={() => setAppearance(appearance === Appearance.LIGHT ? Appearance.DARK : Appearance.LIGHT)}
-          className={styles.appearanceOption}
-          icon={appearance === Appearance.LIGHT ? APPEARANCES[Appearance.DARK].icon : APPEARANCES[Appearance.LIGHT].icon}
-      />
+      </div>
     </div>
   );
 };
 
-const ThemeAppearanceSwitcherButton = ({
-  title,
+const ThemeAppearanceButton = ({
+  label,
   onClick,
-  className,
   icon,
+  className,
+  active=true,
 }: {
-  title: string,
+  label: string,
   onClick: () => void,
-  className: string,
   icon: string,
+  className: string,
+  active?: boolean,
 }) => (
   <div
-      className={styles.themeAppearanceSwitcherButton}
+      className={classNames(styles.themeAppearanceButton, {
+        [styles.active]: active,
+      })}
   >
     <button
-        title={title}
         onClick={onClick}
         className={className}
     >
@@ -112,7 +131,7 @@ const ThemeAppearanceSwitcherButton = ({
     <Text.UltraSmall
         textCase='uppercase'
     >
-      { title }
+      { label }
     </Text.UltraSmall>
   </div>
 );
