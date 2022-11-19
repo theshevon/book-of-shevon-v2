414adb47045a2c707f1aa1abc5f1a9b1a1f9d79e
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-20 02:15:56 +1100

    Add locale context to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './app';
+import { LocaleProvider } from './util/localisation/locale_provider';
 import { ThemeProvider } from './util/theming/theme_provider';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App/>
+      <LocaleProvider>
+        <App/>
+      </LocaleProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
