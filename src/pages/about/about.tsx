6a7565adf67a812fd23d4c44d583b49e7d100c5a
Shevon Mendis <shevonmendis98@gmail.com>
2022-11-22 00:16:12 +1100

    Add welcome message

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { PageContainer } from '../../ui/page_container/page_container';
+import { Text } from '../../ui/text/text';
 import { useLocaleContext } from '../../util/localisation/locale_provider';
 import { useThemeContext } from '../../util/theming/theme_provider';
 import { updateDocumentHeader } from '../../util/title_manager';
@@ -18,7 +19,25 @@ export const About = () => {
   const { theme, appearance } = useThemeContext();
 
   const Content = () => (
-    <Resume/>
+    <>
+      <Text.LargeTitle
+          alignment='centre'
+      >
+        hej 👋🏽
+      </Text.LargeTitle>
+      <Text.LargeTitle
+          alignment='centre'
+      >
+        i&apos;m shevon
+      </Text.LargeTitle>
+      <Text.Small
+          alignment='centre'
+          keepDefaultMargins={true}
+      >
+        anyway here&apos;s my resume if you wanted to know what i&apos;ve been up to professionally:
+      </Text.Small>
+      <Resume/>
+    </>
   );
 
   return (
